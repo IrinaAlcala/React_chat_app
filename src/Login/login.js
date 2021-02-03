14781f8680039e8c64b824f018cbe4d483c2f58b
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2021-02-02 19:14:50 -0800

    added signup page with auth

---
+++ src/Login/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+class loginComponent extends React.Component {
+render() {
+    return(<div>Hello World from Login</div>);
+}
+}
+
+
+
+
+export default loginComponent;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+import LoginComponent from './Login/login';
+import SignupComponent from './Signup/signup';
+import DashboardComponent from './Dashboard/dashboard';
 
-const firebase = require("firebase");
-require("firebase/firestore");
-
-firebase.initializeApp({
+const firebaseConfig = {
 
   apiKey: "AIzaSyBw9roEaK1R5la371-ZmTPjVb1spPgTgIA",
     authDomain: "react-chat-app-43f88.firebaseapp.com",
@@ -15,10 +16,26 @@ firebase.initializeApp({
     storageBucket: "react-chat-app-43f88.appspot.com",
     messagingSenderId: "510348489358",
     appId: "1:510348489358:web:80bbeb4cb96956ebb08fdf",
-})
+    measurementId: "G-RJ47JTQ4CH"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+export default db;
+
+const routing = (
+  <Router>
+    <div id='routing-container'>
+      <Route path='/login' component={LoginComponent}></Route>
+      <Route path='/signup' component={SignupComponent}></Route>
+      <Route path='/dashboard' component={DashboardComponent}></Route>
+    </div>
+  </Router>
+);
+
 
 ReactDOM.render(
-  <div>Hello World</div>,
+ routing,
   document.getElementById('root')
 );
 
